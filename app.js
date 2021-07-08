c0ac21d78fe9ae7c7eb3dc10547995fa345d7bf2
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-08 14:42:06 +0300

    1
    1

---
+++ app.js
@@ -0,0 +1,11 @@
+const item = document.querySelector('.item')
+item.addEventListener('dragstart',dragStart)
+item.addEventListener('dragend', dragEnd)
+
+function dragStart(event) {
+    
+}
+
+function dragEnd() {
+    
+}
