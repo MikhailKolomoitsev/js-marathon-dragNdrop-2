7a7c79589af49bf02ac94534bcd3c0b0cfe63511
mihon.testosterone <mihon.testosterone@gmail.com>
2021-07-08 15:17:26 +0300

    1
    1

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
 item.addEventListener('dragstart',dragStart)
 item.addEventListener('dragend', dragEnd)
 
+placeholders.forEach(placeholder => {
+  placeholder.addEventListener('dragover', dragover)
+  placeholder.addEventListener('dragenter', dragenter)
+  placeholder.addEventListener('dragleave', dragleave)
+  placeholder.addEventListener('drop', dragdrop)
+});
+
 function dragStart(event) {
+    event.currentTarget.classList.add('hold')
+    setTimeout(()=>event.target.classList.add('hide'), 0)
     
 }
 
-function dragEnd() {
-    
+function dragEnd(event) {
+     event.currentTarget.className='item'
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+function dragenter(event) {
+    event.currentTarget.classList.add('hovered')
+}
+function dragleave(event) {
+    event.currentTarget.classList.remove('hovered')
+}
+function dragdrop(event) {
+    event.currentTarget.classList.remove('hovered')
+    event.currentTarget.append(item)
 }
